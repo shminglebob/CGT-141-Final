97cb25edd8e690c02a3842390af61bc8d19d61a3
Jeric Jiang <jian1105@purdue.edu>
2025-12-11 08:58:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from post_reqs import *
 
+with app.app_context():
+    init_md()
+
 if __name__ == '__main__':
     with app.app_context():
-        init_md()
         db.create_all()
     # app.run(host="0.0.0.0")
     app.run(debug=True)
